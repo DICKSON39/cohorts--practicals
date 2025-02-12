ffc03f222b901923b028c73bb8c15e5058a2856c
DICKSON39 <dicksonndumia19@gmail.com>
2025-02-12 08:08:33 +0300

    initial commit

---
+++ HTML AND CSS/calculator/index.js
@@ -0,0 +1,28 @@
+const display = document.getElementById('display');
+function appendToDisplay(input){
+    display.value +=input;
+
+}
+
+function clearDisplay(){
+    display.value = "";
+
+}
+
+function calculate(){
+    try{
+        display.value = eval(display.value);
+
+    }
+    catch(error){
+        display.value = 'SYNTAXERROR'
+    }
+    
+
+}
+
+function backspace() {
+    let display = document.getElementById("display");
+    display.value = display.value.slice(0, -1); // Removes the last character
+}
+
