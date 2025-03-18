7baf66fa79db215981eec2cba94624c474b6ea94
DICKSON39 <dicksonndumia19@gmail.com>
2025-03-18 10:46:42 +0300

    initial commit

--- Angular/learningAngular/src/app/home/home.component.ts
+++ Angular/learningAngular/src/app/home/home.component.ts
@@ -1,22 +1,11 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
-  imports: [FormsModule],
+  imports: [],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
 
-
-  myName= 'John Doe'
-  myAge = 24
-  isDisabled = false
-  value= "This is Angular"
-
-  onSave(){
-    this.isDisabled = true
-  }
-  
 }
